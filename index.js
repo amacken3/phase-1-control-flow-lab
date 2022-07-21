467f3966de139f1021ef618b84ea71cb337595a2
Aengus <aengus.2000@gmail.com>
2022-07-20 22:33:00 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(someValue){
+  let result = [
+  'This one is on me!',
+  'That will be twenty bucks.',
+  'I will gladly take your thirty bucks.',
+  'No can do.'];
+  if (someValue < 400) {
+    return (result[0])
+  }
+  else if (someValue < 2000) {
+    return (result[1])
+  }
+  else if (someValue < 2500) {
+    return (result[2])
+  }
+  else if (someValue > 2500) {
+    return (result[3])
+  }
+};
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(someValue){
+  const cityMessage = someValue === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return cityMessage;
+};
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(someValue) {
+  let message;
+  switch (someValue) {
+    case 'generous' :
+      message = 'Thank you so much.';
+      break;
+    case 'not as generous' :
+      message = 'Thank you.';
+      break;
+    case 'thanks for everything' :
+      message = 'Bye.';
+      break;
+  }
+  return message;
+};
